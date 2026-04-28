4718ca752d9044738151aec464d2b27ec22c3066
Ivan Lashkevich <swxxrd@gmail.com>
2026-04-27 23:31:58 -0700

    FIX: attempt to fix CI/CD pipeline

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,21 @@
+
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
-@app.get("/api/cars")
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://css360.qeax.cloud"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/cars")
 async def get_cars():
     # temporary fake car list
     return [
@@ -10,3 +23,7 @@ async def get_cars():
         {"id": 2, "brand": "Audi", "model": "RS6", "price": 85000},
         {"id": 3, "brand": "Mercedes", "model": "C63", "price": 60000}
     ]
+    
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
